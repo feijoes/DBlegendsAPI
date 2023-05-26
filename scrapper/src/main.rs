d3579d6f35e4f80d26c08d9286b8707c2ba4a726
Pedro Calado Moura <pcaladomoura@gmail.com>
2023-05-25 21:22:49 -0300

    add initial selenium for scraper

--- scrapper/src/main.rs
+++ scrapper/src/main.rs
@@ -1,14 +1,43 @@
+use headless_chrome::browser::context::Context;
+use headless_chrome::browser::tab::Tab;
+use headless_chrome::protocol::page::ScreenshotFormat;
+use std::error::Error;
+use tokio::runtime::Runtime;
+
+fn scrape_page_with_js(url: &str) -> Result<(), Box<dyn Error>> {
+    // Create a new runtime
+    let mut rt = Runtime::new().unwrap();
+
+    // Create a new browser context
+    let browser = rt.block_on(headless_chrome::Browser::new())?;
+    let context = rt.block_on(browser.new_context())?;
+
+    // Create a new tab in the context
+    let tab = rt.block_on(context.new_tab())?;
+
+    // Enable JavaScript on the tab
+    rt.block_on(tab.enable_javascript())?;
+
+    // Navigate to the desired URL
+    rt.block_on(tab.navigate_to(url.into()))?;
+
+    // Wait for JavaScript execution to complete (adjust the delay as needed)
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    // Take a screenshot of the page
+    let screenshot = rt.block_on(tab.capture_screenshot(ScreenshotFormat::PNG))?;
+
+    // Save the screenshot to a file named "index.html"
+    std::fs::write("index.html", screenshot)?;
+
+    Ok(())
+}
+
 fn main() {
-    let response = reqwest::blocking::get(
-        "https://legends.dbz.space/characters/",
-    )
-    .unwrap()
-    .text()
-    .unwrap();
-    let document = scraper::Html::parse_document(&response);
-    let select_char =  scraper::Selector::parse("chara list").unwrap();
+    let url = "https://example.com";
 
-    let list_char = document.select(&select_char);
-    println!("{:?}",list_char)
-
+    match scrape_page_with_js(url) {
+        Ok(()) => println!("Page scraped successfully!"),
+        Err(err) => eprintln!("An error occurred: {:?}", err),
+    }
 }
